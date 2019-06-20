9433bca3c9e39937d45690d2e72da5dd2526ffe1
tamakiii <tamakiii@users.noreply.github.com>
2019-06-20 22:38:54 +0900

    using react, react-dom

--- src/index.tsx
+++ src/index.tsx
@@ -1,2 +1,16 @@
-const root = document.querySelector("#root") as HTMLDivElement;
-root.textContent = "Hello, Parcel";
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+
+const App = () => (
+  <>
+    <h1>Test Parcel</h1>
+    <ul>
+      <li>hi</li>
+    </ul>
+  </>
+)
+
+ReactDOM.render(
+  <App />,
+  document.querySelector("#root") as HTMLDivElement,
+)

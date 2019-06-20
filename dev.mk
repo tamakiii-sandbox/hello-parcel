3979276661773a47a94173e92568d0ae63eb8120
tamakiii <tamakiii@users.noreply.github.com>
2019-06-20 23:02:56 +0900

    split into two Makefile

--- dev.mk
+++ dev.mk
@@ -1,15 +1,8 @@
-
-@dev:
-	npx parcel src/index.html
-
 @build:
-	npx parcel build src/index.html
+	npx parcel src/index.html
 
 @watch:
 	npx parcel watch src/index.html
 
 @test:
-	npx jest
-
-@test-dev:
 	npx jest --watch
